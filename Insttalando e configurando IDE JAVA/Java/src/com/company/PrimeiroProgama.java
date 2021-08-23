d7676462e9add23e8380a8a46d423e600ef75c4b
Arilson Martins <39891377+arilson-martins@users.noreply.github.com>
2021-08-23 16:29:38 -0300

    Add files via upload

---
+++ Insttalando e configurando IDE JAVA/Java/src/com/company/PrimeiroProgama.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.model.gato;
+
+public class PrimeiroProgama {
+
+    public static void main(String[] args) {
+        System.out.println("ola mundo ! ");
+        gato gato = new gato();
+        System.out.println(gato);
+    }
+}
